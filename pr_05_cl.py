62e4c91a781f8c79be32a3c0472261eeeab7e4c1
Carlos <carlosbda99@hotmail.com>
2017-06-22 18:51:11 -0300

    Versao 1.1

--- pr_05_cl.py
+++ pr_05_cl.py
@@ -3,19 +3,18 @@
 import socket
 
 HOST = socket.gethostbyname('localhost')
-PORT = 3001
+PORT = 3000
 
 tcp_sc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 tcp_sc.connect((HOST,PORT))
 
-while True:
-    message = input()
-    message = message.encode('utf-8')
-    tcp_sc.send(message)
-    
-    message = tcp_sc.recv(1024)
-    message = message.decode('utf-8')
-    print ('Resultado: ', message)
+message = input()
+message = message.encode('utf-8')
+tcp_sc.send(message)
+
+message = tcp_sc.recv(2048)
+message = message.decode('utf-8')
+print (message)
 
 tcp_sc.close()
--- pr_05_serv.py
+++ pr_05_serv.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''Faça o teste com
+soma 3 bola\nsoma 2 3\nraiz_quadrada 9
+Assim foi pedido, comando + arg +\n = varias solicitacoes
+'''
+
 
 import socket
 from math import sqrt
@@ -16,7 +21,7 @@ class pr5 ():
         return str(x)
 
 HOST = socket.gethostbyname('localhost')
-PORT = 3001
+PORT = 3000
 
 tcp_ss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 tcp_ss.bind((HOST,PORT))
@@ -27,19 +32,24 @@ func = pr5()
 client, addr = tcp_ss.accept()
 
 def send_msg(msg):
-    message = msg
-    message = msg.encode('utf-8')
-    client.send(message)
+	message = msg
+	message = msg.encode('utf-8')
+	client.send(message)
 
-while True:
-    message = client.recv(1024)
-    message = message.decode('utf-8')
+message = client.recv(1024)
+message = message.decode('utf-8')
 
-    wtodo = message.split(' ')
-    if wtodo[0] == 'soma':
-        send_msg(func.soma(wtodo[1],wtodo[2]))
-    elif wtodo[0] == 'raiz_quadrada':
-        send_msg(func.raiz_quadrada(wtodo[1]))
-    else:
-        send_msg('Mensagem invalida')
+inst = message.split('\\n')
+resp = ''
+print (message)
+
+for x in inst:
+	wtodo = x.split(' ')
+	if wtodo[0] == 'soma' and wtodo[1].isnumeric() and wtodo[2].isnumeric():
+		resp += ('Resultado de %s + %s: '%(wtodo[1],wtodo[2])) + (func.soma(wtodo[1],wtodo[2])) + '\n'
+	elif wtodo[0] == 'raiz_quadrada' and wtodo[1].isnumeric():
+		resp += ('Resultado da raiz de %s: '%wtodo[1]) + (func.raiz_quadrada(wtodo[1])) + '\n'
+	else:
+		resp += 'Resultado: ' + ('Mensagem invalida') + '\n'
+send_msg(resp)
 tcp_ss.close()
